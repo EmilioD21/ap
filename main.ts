eaa44d5359432207e52b500a4b6b8a3d139189e0
EmilioD21 <156109686+EmilioD21@users.noreply.github.com>
2024-03-05 10:46:38 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,64 @@
-
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 2 2 2 2 2 2 2 2 . . 
+        . . . . . 2 c 2 2 2 2 2 2 4 2 . 
+        . . . . 2 c c 2 2 2 2 2 2 4 c 2 
+        . . d 2 4 c c 2 4 4 4 4 4 4 c c 
+        . d 2 2 4 c b e e e e e e e 2 c 
+        . 2 2 2 4 b e e b b b e b b e 2 
+        . 2 2 2 2 2 e b b b b e b b b e 
+        . 2 2 2 2 e 2 2 2 2 2 e 2 2 2 e 
+        . 2 d d 2 e f e e e f e e e e e 
+        . d d 2 e e e f e e f e e e e e 
+        . e e e e e e e f f f e e e e e 
+        . e e e e f f f e e e e f f f f 
+        . . . e f f f f f e e f f f f f 
+        . . . . f f f f . . . . f f f . 
+        . . . . . . . . . . . . . . . . 
+        `)
+})
+let vel = 0
+let direct = 0
+let mySprite: Sprite = null
+tiles.setCurrentTilemap(tilemap`level1`)
+let turn_vel = 0.2
+let acc = 200
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . 2 2 2 2 2 2 2 2 . . . . 
+    . . . 2 4 2 2 2 2 2 2 c 2 . . . 
+    . . 2 c 4 2 2 2 2 2 2 c c 2 . . 
+    . 2 c c 4 4 4 4 4 4 2 c c 4 2 d 
+    . 2 c 2 e e e e e e e b c 4 2 2 
+    . 2 2 e b b e b b b e e b 4 2 2 
+    . 2 e b b b e b b b b e 2 2 2 2 
+    . e e 2 2 2 e 2 2 2 2 2 e 2 2 2 
+    . e e e e e e f e e e f e 2 d d 
+    . e e e e e e f e e f e e e 2 d 
+    . e e e e e e f f f e e e e e e 
+    . e f f f f e e e e f f f e e e 
+    . . f f f f f e e f f f f f e . 
+    . . . f f f . . . . f f f f . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+scene.cameraFollowSprite(mySprite)
+game.onUpdate(function () {
+    let top_vel = 0
+    if (controller.right.isPressed()) {
+        direct += turn_vel * (1 - vel / (1.5 * top_vel))
+    }
+    if (controller.left.isPressed()) {
+        direct += 0 * (turn_vel * (1 - vel / (1.5 * top_vel)))
+    }
+    if (controller.A.isPressed()) {
+        acc = 1.5
+    } else if (controller.down.isPressed()) {
+        acc = -2.5
+    } else {
+        acc = -0.5
+    }
+    vel = Math.constrain(vel + acc, 0, top_vel)
+    mySprite.vx = Math.cos(direct * vel)
+    mySprite.vy = Math.sin(direct) * vel
+})
